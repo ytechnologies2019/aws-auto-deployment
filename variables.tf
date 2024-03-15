faf877b155e4021816597ca7ce2833fcfbfe779b
Y-Technologies <127391282+ytechnologies2019@users.noreply.github.com>
2024-03-16 00:53:46 +0630

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
+#STG_Region
+variable "stg_aws_region" {
+  type = string
+}
+
+#Prod_Region
+variable "prod_aws_region" {
+  type = string
+}
